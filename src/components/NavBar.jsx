8cce9679476c99c98d144d23e5bcf283d41b7f03
Shariq Bin Shoaib <sbs_ansari@outlook.com>
2020-08-17 20:54:15 +0500

    initial commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+
+function NavBar() {
+    return (
+        <>
+            <h1>NavBar</h1>
+            <Button>Hello</Button>
+        </>
+    )
+}
+
+export default NavBar;
